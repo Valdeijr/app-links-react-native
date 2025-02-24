a4ea8a5f7a0cd87a42ff9c4f798bb5f094a11e4b
Valdeijr <valdei.ferreira.jr@gmail.com>
2025-02-24 17:39:29 -0300

    UpdateStyles

---
+++ src/constants/geometries.ts
@@ -0,0 +1,19 @@
+export const geometries = {
+	spacing: {
+		tiny: 4,
+		small: 8,
+		compact: 12,
+		regular: 16,
+		medium: 20,
+		large: 24,
+		immense: 32,
+	},
+	rounded: {
+		tiny: 4,
+		small: 8,
+		compact: 12,
+		regular: 16,
+		medium: 24,
+		large: 48,
+	},
+};
--- src/theme/global.ts
+++ src/theme/global.ts
@@ -4,4 +4,12 @@ export const globalStyles = StyleSheet.create({
 	container: {
 		flex: 1,
 	},
+	content: {},
+	header: {
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-between",
+		paddingHorizontal: 20,
+		width: "100%",
+	},
 });
